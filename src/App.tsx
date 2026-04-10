039eaf681d7e5f8b027afb91b42aa7dcff2ee6ab
mauricioxDxP <calderonxdp@hotmail.com>
2026-04-10 13:32:33 -0400

    lazy loading fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { Layout } from './components/Layout';
-import { DashboardPage } from './pages/DashboardPage';
-import { CategoriasPage } from './pages/CategoriasPage';
-import { MotivosPage } from './pages/MotivosPage';
-import { TransaccionesPage } from './pages/TransaccionesPage';
-import { ConfiguracionPage } from './pages/ConfiguracionPage';
-import { ReportesPage } from './pages/ReportesPage';
+import { Loading } from './components/UI';
+
+// Lazy loading de páginas
+const DashboardPage = lazy(() => import('./pages/DashboardPage').then(m => ({ default: m.DashboardPage })));
+const CategoriasPage = lazy(() => import('./pages/CategoriasPage').then(m => ({ default: m.CategoriasPage })));
+const MotivosPage = lazy(() => import('./pages/MotivosPage').then(m => ({ default: m.MotivosPage })));
+const TransaccionesPage = lazy(() => import('./pages/TransaccionesPage').then(m => ({ default: m.TransaccionesPage })));
+const ConfiguracionPage = lazy(() => import('./pages/ConfiguracionPage').then(m => ({ default: m.ConfiguracionPage })));
+const ReportesPage = lazy(() => import('./pages/ReportesPage').then(m => ({ default: m.ReportesPage })));
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Layout>
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/categorias" element={<CategoriasPage />} />
-            <Route path="/motivos" element={<MotivosPage />} />
-            <Route path="/transacciones" element={<TransaccionesPage />} />
-            <Route path="/reportes" element={<ReportesPage />} />
-            <Route path="/configuracion" element={<ConfiguracionPage />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/categorias" element={<CategoriasPage />} />
+              <Route path="/motivos" element={<MotivosPage />} />
+              <Route path="/transacciones" element={<TransaccionesPage />} />
+              <Route path="/reportes" element={<ReportesPage />} />
+              <Route path="/configuracion" element={<ConfiguracionPage />} />
+            </Routes>
+          </Suspense>
         </Layout>
       </BrowserRouter>
     </ThemeProvider>
